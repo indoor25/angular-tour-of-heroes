777de5c32bbab77e282934705c2c1a403511bce2
indoor25 <indoor1980@gmail.com>
2023-08-22 11:46:40 +0200

    Display a list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {HeroesComponent} from './heroes/heroes/heroes.component';
+import {HeroesComponent} from './heroes/heroes.component';
 import {FormsModule} from "@angular/forms";
 
 @NgModule({
